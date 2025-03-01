83e732efac1cb42cb9f879b0b9c51f3d8a8d2f98
Extraordinary <3023538364@qq.com>
2025-03-02 03:30:09 +0800

    Update install_ssh_server.sh

--- install_ssh_server.sh
+++ install_ssh_server.sh
@@ -16,7 +16,6 @@ sudo apt install openssh-server -y
 # SSH Config
 sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 sudo echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-sudo echo "AuthorizedKeysFile /kaggle/working/.ssh/authorized_keys" >> /etc/ssh/sshd_config
 sudo echo "PubkeyAuthentication no" >> /etc/ssh/sshd_config
 
 sudo service ssh restart
