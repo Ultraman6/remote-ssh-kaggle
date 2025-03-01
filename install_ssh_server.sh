63906675b05ce75d46750929e05070cd154a12cd
Extraordinary <3023538364@qq.com>
2025-03-02 03:26:48 +0800

    Update install_ssh_server.sh

--- install_ssh_server.sh
+++ install_ssh_server.sh
@@ -1,19 +1,4 @@
 #!/bin/bash#
-# Setup public - private key
-mkdir -p /kaggle/working/.ssh
-echo $1
-FILE=/kaggle/working/.ssh/authorized_keys
-if test -f "$FILE";
-then
-    wget $1 -O /kaggle/working/.ssh/temp
-    cat /kaggle/working/.ssh/temp >> /kaggle/working/.ssh/authorized_keys
-    rm /kaggle/working/.ssh/temp
-else
-    wget $1 -O /kaggle/working/.ssh/authorized_keys
-fi
-
-chmod 700 /kaggle/working/.ssh
-chmod 600 /kaggle/working/.ssh/authorized_keys
 
 # Download ngrok
 FILE=/kaggle/working/SSH/ngrok
@@ -30,8 +15,8 @@ sudo apt install openssh-server -y
 
 # SSH Config
 sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-sudo echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+sudo echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 sudo echo "AuthorizedKeysFile /kaggle/working/.ssh/authorized_keys" >> /etc/ssh/sshd_config
-sudo echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
+sudo echo "PubkeyAuthentication no" >> /etc/ssh/sshd_config
 
 sudo service ssh restart
