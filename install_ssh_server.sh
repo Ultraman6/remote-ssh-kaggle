f1de94d316bf58e5be95de0aac7d36692ea18ec9
Extraordinary <3023538364@qq.com>
2025-03-02 02:47:06 +0800

    Update install_ssh_server.sh

--- install_ssh_server.sh
+++ install_ssh_server.sh
@@ -3,7 +3,7 @@
 mkdir -p /kaggle/working/.ssh
 echo $1
 FILE=/kaggle/working/.ssh/authorized_keys
-if test -f "$FILE"; 
+if test -f "$FILE";
 then
     wget $1 -O /kaggle/working/.ssh/temp
     cat /kaggle/working/.ssh/temp >> /kaggle/working/.ssh/authorized_keys
@@ -17,7 +17,7 @@ chmod 600 /kaggle/working/.ssh/authorized_keys
 
 # Download ngrok
 FILE=/kaggle/working/SSH/ngrok
-if ! test -f "$FILE"; 
+if ! test -f "$FILE";
 then
     wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
     sudo tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
