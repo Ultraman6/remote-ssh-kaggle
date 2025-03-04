3b4005dcaafae8becca6a7d69f031c450af148c8
Extraordinary <3023538364@qq.com>
2025-03-05 00:03:06 +0800

    Update deploy_ssh.sh

--- deploy_ssh.sh
+++ deploy_ssh.sh
@@ -1,15 +1,18 @@
 #!/bin/bash#
 
 # Setup authenticational
-echo root:$1 | chpasswd
+sudo useradd xyz
+echo xyz:$1 | sudo chpasswd
+sudo usermod -aG sudo xyz
 
 # Install SSH-Server
 sudo apt update
 sudo apt install openssh-server -y
 
 # SSH Config
-sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+sudo echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 sudo echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 sudo echo "PubkeyAuthentication no" >> /etc/ssh/sshd_config
+sudo echo "AllowUsers xyz" | sudo tee -a /etc/ssh/sshd_config
 
 sudo service ssh restart
