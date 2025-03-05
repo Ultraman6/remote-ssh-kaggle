62d69b794f68cb377792ffd052981447cedc5f90
Extraordinary <3023538364@qq.com>
2025-03-05 21:30:07 +0800

    Update deploy_ssh.sh

--- deploy_ssh.sh
+++ deploy_ssh.sh
@@ -17,7 +17,5 @@ echo sudo "X11Forwarding yes" >> /etc/ssh/sshd_config
 echo sudo "X11DisplayOffset 10" >> /etc/ssh/sshd_config
 echo sudo "IgnoreRhosts yes" >> /etc/ssh/sshd_config
 echo sudo "HostbasedAuthentication no" >> /etc/ssh/sshd_config
-echo sudo "PrintLastLog yes" >> /etc/ssh/sshd_config
-echo sudo "AcceptEnv LANG LC_*" >> /etc/ssh/sshd_config
 
 sudo service ssh restart
