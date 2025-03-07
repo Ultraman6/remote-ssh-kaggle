bc9df3ab75f89ef9bb1eaec7c65b68439862a67e
Extraordinary <3023538364@qq.com>
2025-03-07 23:40:45 +0800

    Update deploy_ssh.sh

--- deploy_ssh.sh
+++ deploy_ssh.sh
@@ -8,8 +8,16 @@ sudo apt update
 sudo apt install openssh-server -y
 
 # SSH Config
-sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-sudo echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-sudo echo "PubkeyAuthentication no" >> /etc/ssh/sshd_config
+echo "Protocol 2" >> /etc/ssh/sshd_config
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+echo "TCPKeepAlive yes" >> /etc/ssh/sshd_config
+echo "X11Forwarding yes" >> /etc/ssh/sshd_config
+echo "X11DisplayOffset 10" >> /etc/ssh/sshd_config
+echo "PubkeyAuthentication no" >> /etc/ssh/sshd_config
+echo "IgnoreRhosts yes" >> /etc/ssh/sshd_config
+echo "HostbasedAuthentication no" >> /etc/ssh/sshd_config
+echo "PrintLastLog yes" >> /etc/ssh/sshd_config
+echo "AcceptEnv LANG LC_*" >> /etc/ssh/sshd_config
 
 sudo service ssh restart
